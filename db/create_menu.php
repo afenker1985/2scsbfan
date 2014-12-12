6ff5553266f7134e99e598dd81cde3c7f2d606a5
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 11:35:42 -0600

    db query check

--- db/create_menu.php
+++ db/create_menu.php
@@ -1,17 +1,29 @@
 <?php
-class MyDB extends SQLite3
-{
-   function __construct()
-   {
-      $this->open('2scsb.db');
-   }
-}
+	class MyDB extends SQLite3
+	{
+	   function __construct()
+	   {
+	      $this->open('2scsb.db');
+	   }
+	}
 
-$db = new MyDB();
+	$db = new MyDB();
 
-if(!$db){
-   echo $db->lastErrorMsg();
-} else {
-   echo "Opened database successfully\n";
+	if(!$db){
+	   echo $db->lastErrorMsg();
+	} else {
+		$sql =<<<EOF
+			SELECT slug FROM albums WHERE is_active=1;
+EOF;
+
+	$result = $db->query($sql);
+	
+	if(!$result){
+	   echo $db->lastErrorMsg();
+	} else {
+		echo "Query run properly";
+	}
+	
+	$db->close();
 }
 ?>
