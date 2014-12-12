9c0c9f60aed3737942a059ee6fceb11fcaa544d3
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 11:11:55 -0600

    attempt 5 to get db query

--- db/create_menu.php
+++ db/create_menu.php
@@ -1,10 +1,10 @@
 <?php
 
-$db = sqlite_open('2scsb.db', 0666);
+$db = new SQLite3('2scsb.db');
 
-$result = sqlite_query($db, 'SELECT slug FROM albums');
+$result = $db->query('SELECT slug FROM albums');
 
 echo "PASS";
-var_dump(sqlite_fetch_array($result);
+var_dump($result->fetchArray());
 
 ?>
