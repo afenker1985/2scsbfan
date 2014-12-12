22e33b760b2f3fcb7a6830e23f8a2ce92802dad0
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 10:56:36 -0600

    vardump db result

--- db/create_menu.php
+++ db/create_menu.php
@@ -7,7 +7,7 @@ class MyDB extends SQLite3 {
 	}
 }
 
-$db = new MyDB();
+$db = new SQLite3('2scsb.db');
 
 $result = $db->query('SELECT * FROM albums WHERE is_active = 1 ORDER BY release_date ASC');
 
