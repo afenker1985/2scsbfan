9929a8acf16a4e632bf5d91dbec8cf0b519b4768
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 20:33:17 -0600

    fixed album image path | final push

--- album.php
+++ album.php
@@ -1,6 +1,6 @@
 	<article id="latest-article" class="container" style="min-height: 400px;">
 		  <h2><?=$id?></h2>
 		  <div class="dcontentc cf">
-			  <img src="imgs/<?=$id?>.jpg" />
+			  <img src="<?$path?>imgs/<?=$id?>.jpg" />
 		  </div>
 	  </article>
