604ff73299295ab151b95bb95536360cfb6f10d2
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 07:56:34 -0600

    Added album_info.php that pulls infrom from DB based on slug

--- db/album_info.php
+++ db/album_info.php
@@ -1,20 +1,12 @@
 <?php
-	class MyDB extends SQLite3
-	{
-	   function __construct()
-	   {
-		   $this->open($_ENV['OPENSHIFT_DATA_DIR'] . '/2scsb.db');
-		   $this->lastErrorMsg();
-	   }
-	}
-	
+
 	$db = new MyDB();
 
 	if(!$db){
 	   echo $db->lastErrorMsg();
 	} else {
 
-		$result = $db->query('SELECT * FROM albums WHERE slug='. $id .' ORDER BY release_date ASC');
+		$result = $db->query('SELECT * FROM albums WHERE slug=' . $id . ' ORDER BY release_date ASC');
 	
 		if(!$result){
 		   echo $db->lastErrorMsg();
