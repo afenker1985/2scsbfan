874cdf68597bfc1a60482cf6289e23442af01831
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 08:11:38 -0600

    Fixed subtitle check in album.php

--- album.php
+++ album.php
@@ -1,7 +1,7 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'db/album_info.php'; ?>	
 	<article id="latest-article" class="container" style="min-height: 400px;">
 		  <h2><?=$row['title']?></h2>
-		  <?php if ($row['subtitle'] != NULL): ?>
+		  <?php if ($row['subtitle'] != 'NULL'): ?>
 			  <h4><?=$row['subtitle']?></h4>
 		  <?php endif; ?>
 		  <div class="dcontentc cf">
