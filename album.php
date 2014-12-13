1946fb49de395ab19bae2491dc3ab97d9f398f95
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 07:50:46 -0600

    Added album_info.php that pulls infrom from DB based on slug

--- album.php
+++ album.php
@@ -1,6 +1,6 @@
 <?php	include $_SERVER['DOCUMENT_ROOT'].'/db/album_info.php'; ?>	
 	<article id="latest-article" class="container" style="min-height: 400px;">
-		  <h2><?=$row['title']?></h2>
+		  <h2><?=$id?></h2>
 		  <div class="dcontentc cf">
 			  <img src="<?=$path?>imgs/<?=$id?>.jpg" width="280"/>
 		  </div>
