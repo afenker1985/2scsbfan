c7a839573ce81044d34e1f8ee54a1a30bbdb0482
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 19:13:56 -0600

    Added album.php

--- album.php
+++ album.php
@@ -1,5 +1,5 @@
 	<article id="latest-article" class="container" style="min-height: 400px;">
-		  <h2><?=$album_title; ?></h2>
+		  <h2><?=$id?></h2>
 		  <div class="dcontentc cf">
 			  <img src="imgs/<?=$id?>.jpg" />
 		  </div>
