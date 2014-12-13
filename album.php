6f00952c652c8d12903915fec1ad98ea09d9b02d
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 07:29:25 -0600

    Added album_info.php that pulls infrom from DB based on slug

--- album.php
+++ album.php
@@ -1,5 +1,6 @@
+<? include $path . 'album_info.php'; ?>
 	<article id="latest-article" class="container" style="min-height: 400px;">
-		  <h2><?=$id?></h2>
+		  <h2><?=$row['title']?></h2>
 		  <div class="dcontentc cf">
 			  <img src="<?=$path?>imgs/<?=$id?>.jpg" width="280"/>
 		  </div>
---
+++ db/album_info.php
@@ -0,0 +1,30 @@
+<?php
+	class MyDB extends SQLite3
+	{
+	   function __construct()
+	   {
+		   $this->open($_ENV['OPENSHIFT_DATA_DIR'] . '/2scsb.db');
+	   }
+	}
+
+	$db = new MyDB();
+
+	if(!$db){
+	   echo $db->lastErrorMsg();
+	} else {
+
+		$result = $db->query('SELECT * FROM albums WHERE slug='. $id .' ORDER BY release_date ASC');
+	
+		if(!$result){
+		   echo $db->lastErrorMsg();
+		} else {
+			$album_list = '';
+			while($row=$result->fetchArray(SQLITE3_ASSOC)) {
+				print_r($row);
+			}
+
+		}
+	
+		$db->close();
+	}
+?>
