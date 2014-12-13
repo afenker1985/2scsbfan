91cf42296cbe4f51ab508c3180cc2ff762c6adfb
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 08:09:58 -0600

    Added album_info.php that pulls infrom from DB based on slug

--- album.php
+++ album.php
@@ -7,7 +7,12 @@
 		  <div class="dcontentc cf">
 			  <div class="album_info">
 				  <img src="<?=$path?>imgs/<?=$id?>.jpg" width="280"/>
-				  <strong>Release Date:</strong> <?=date("F d, Y", strtotime($row['release_date']))?>
+				  <br />
+				  <strong>Release Date:</strong> <?=date("F j, Y", strtotime($row['release_date']))?>
+				  <br />
+				  © <?=$row['copyright']?> <?=$row['label']?>
+				  <br />
+				  <strong>Album Length:</strong> <?=gmdate("H:i:s", $row['total_length'])?>
 			  </div>
 		  </div>
 	  </article>
