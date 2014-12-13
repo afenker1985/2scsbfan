75334505d58e3f4d43eda49d26064d8165eed0a9
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 18:17:15 -0600

    create active dropdown menu

--- db/create_menu.php
+++ db/create_menu.php
@@ -22,12 +22,13 @@ EOF;
 		if(!$result){
 		   echo $db->lastErrorMsg();
 		} else {
+			print_r($result->fetchArray());
+			
 			$album_list = '';
 			foreach ($result as $row) {
 				$album_list .= '<li><a href="/albums/' . $row[0] . '"><span>- </span>' . $row[2] . '</a></li>'
 			}
 			print_r($album_list);
-			print_r($result->fetchArray());
 		}
 	
 		$db->close();
