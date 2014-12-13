add35fbb92147b46660fdbf72b4a5c291d6830e4
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 13:11:24 -0600

    scsb debugging

--- db/scsb.php
+++ db/scsb.php
@@ -50,10 +50,7 @@
 				
 				$r = $this->db->query('SELECT title, track_number, song_length FROM songs WHERE song_id=' . $row['song_id']);
 				
-				$track_list[$i] = $r->fetchArray(SQLITE3_ASSOC);
-				
-				print_r($row);
-				
+				$track_list[$i] = $r->fetchArray(SQLITE3_ASSOC);				
 				$i++;
 			}
 			
