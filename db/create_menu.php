afd6c7192f0deddaa96bb2105b182536bf0924a9
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-12 18:19:37 -0600

    create active dropdown menu

--- db/create_menu.php
+++ db/create_menu.php
@@ -22,11 +22,11 @@ EOF;
 		if(!$result){
 		   echo $db->lastErrorMsg();
 		} else {
-			print_r($result->fetchArray());
+			print_r($result->fetchArray(SQLITE3_ASSOC));
 			
 			$album_list = '';
 			foreach ($result as $row) {
-				$album_list .= '<li><a href="/albums/' . $row[0] . '"><span>- </span>' . $row[2] . '</a></li>';
+				$album_list .= '<li><a href="/albums/' . $row['slug'] . '"><span>- </span>' . $row['title'] . '</a></li>';
 			}
 			print_r($album_list);
 		}
