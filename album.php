6bad578546b8d84eea22ba52298359a874854e48
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-13 22:59:06 -0600

    update track list css

--- album.php
+++ album.php
@@ -4,7 +4,7 @@
     $('#accordion').find('.accordion-toggle').click(function(){
 
       //Expand or collapse this panel
-      $(this).next().slideToggle('fast');
+      $(this).next().slideToggle('slow');
 
       //Hide the other panels
       $(".accordion-content").not($(this).next()).slideUp('fast');
