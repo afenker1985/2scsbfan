0da4b34bc39be3bba409f76fc2606709020a3bee
Aaron Fenker <aaronfenker@Aarons-MacBook-Air.local>
2014-12-15 14:22:33 -0600

    updated scsb

--- db/scsb.php
+++ db/scsb.php
@@ -62,7 +62,7 @@
 				
 				$r = $this->db->query("SELECT title, song_length, track_number, song_id FROM songs WHERE song_id=" . $row['song_id']);
 				
-				if(!$r){
+				if(!$r->fetchArray(SQLITE3_ASSOC)) {
 				   echo "Error 1: " . $this->db->lastErrorMsg() . "<br />";
 				} else {
 				
